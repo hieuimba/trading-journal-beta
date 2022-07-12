fe21312d9d2f3331e029b420c9df22eb537bb888
Hieu Ngo <89481020+hieuimba@users.noreply.github.com>
2022-07-12 10:43:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,7 +172,7 @@ if watchlist_send:
 
 trades_sorted = trades.sort_values(by=['OpenDate'], ascending=False)
 trades_sorted = trades_sorted['PnL']
-trades_sorted['CumPnL'] = trades_sorted['PnL'].cumsum()
+trades_sorted['CumPnL'] = trades_sorted.cumsum()
 st.dataframe(trades_sorted)
 st.line_chart(trades_sorted)
 # send_update_request = st.button('Send Update Request')
